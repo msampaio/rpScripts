a29f95c65c1c2bf0a0d8e8825d63e8e600ff699b
Marcos da Silva Sampaio <m@git.sampaio.me>
2023-02-09 08:21:49 -0300

    Refactoring with plotter classes

--- src/rpp.py
+++ src/rpp.py
@@ -74,149 +74,177 @@ def invert_dataframe(df):
     return inverted
 
 
-def plot_simple_partitiogram(df, img_format='svg', with_labels=True, outfile=None):
-    seq = [
-        [partition, len(_df), _df.Agglomeration.iloc[0], _df.Dispersion.iloc[0]]
-        for partition, _df in df.groupby('Partition')
-    ]
-    columns=['Partition', 'Quantity', 'Agglomeration', 'Dispersion']
-    df = pandas.DataFrame(seq, columns=columns)
+class AbstractPlotter(object):
+    def __init__(self, dataframe, image_format='svg', basename=None) -> None:
+        self.dataframe = dataframe
+        self.image_format = image_format
+        self.basename = basename
+        self.outfile = '{}-{}.{}'.format(self.basename, self.name, self.image_format)
+        self.name = None
 
-    plt.clf()
-    ax = df.plot(
-        grid=True,
-        kind='scatter',
-        x='Agglomeration',
-        y='Dispersion',
-    )
+    def plot(self):
+        pass
 
-    if with_labels:
-        factor = 1.025
-        fontsize = 12
+    def xticks_adjust(self):
+        plt.xticks(rotation=90)
+        plt.tight_layout()
 
-        for _, s in df.iterrows():
-            x = s['Agglomeration']
-            y = s['Dispersion']
-            v = s['Partition']
-            plt.text(x * factor, y * factor , v, fontsize=fontsize)
-
-    if img_format == 'svg':
-        plt.savefig(outfile)
-    else:
-        plt.savefig(outfile, dpi=RESOLUTION)
-    plt.close()
+    def save(self):
+        if self.image_format == 'svg':
+            plt.savefig(self.outfile)
+        else:
+            plt.savefig(self.outfile, dpi=RESOLUTION)
+        plt.close()
 
 
-def plot_simple_indexogram(df, img_format='svg', outfile=None, close_bubbles=False):
-    def draw_vertical_line(row, x):
-        ymin = row[1].Agglomeration * -1
-        ymax = row[1].Dispersion
-        plt.vlines(x=x, ymin=ymin, ymax=ymax, linestyles='dotted', colors='C3')
-
-    inverted = invert_dataframe(df)
-
-    plt.clf()
-
-    ax = inverted.plot(grid=True)
-    ax.set_ylabel('Values\n<- aggl./disp. ->')
-    ax.set_xlabel('Positions (measure, offset)')
-
-    # draw vertical lines to close the bubbles
-    if close_bubbles:
-        rest_segment = False
-        last_row = None
-        for i, row in enumerate(df.iterrows()):
-            _agg = row[1].Agglomeration
-            if pandas.isnull(_agg):
-                if not rest_segment:
-                    if last_row:
-                        x = i - 1
-                        draw_vertical_line(last_row, x)
-                    rest_segment = True
-            else:
-                if rest_segment:
-                    x = i
-                    draw_vertical_line(row, x)
-                    rest_segment = False
-            last_row = row
-
-    plt.xticks(rotation=90)
-    plt.tight_layout()
-
-    if img_format == 'svg':
-        plt.savefig(outfile)
-    else:
-        plt.savefig(outfile, dpi=RESOLUTION)
-    plt.close()
+class AbstractPartitiogramPlotter(AbstractPlotter):
+    def __init__(self, dataframe, image_format='svg', basename=None, with_labels=True) -> None:
+        super().__init__(dataframe, image_format, basename)
+        self.with_labels = with_labels
 
 
-def plot_stem_indexogram(df, img_format='svg', outfile=None):
-    inverted = invert_dataframe(df)
-
-    ind = ['({}, {})'.format(a, b) for a, b in inverted.index.values]
-    size = len(ind)
-    step = int(size / 8)
-
-    plt.clf()
-    plt.stem(ind, inverted.Dispersion.values, markerfmt=' ')
-    plt.stem(ind, inverted.Agglomeration.values, markerfmt=' ', linefmt='C1-')
-    plt.xticks(range(0, size, step))
-    plt.xlabel('Positions (measure, offset)')
-    plt.ylabel('Values\n<- aggl./disp. ->')
-    plt.grid()
-    plt.legend(inverted.columns)
-    plt.xticks(rotation=90)
-    plt.tight_layout()
-
-    if img_format == 'svg':
-        plt.savefig(outfile)
-    else:
-        plt.savefig(outfile, dpi=RESOLUTION)
-    plt.close()
+class AbstractIndexogramPlotter(AbstractPlotter):
+    def __init__(self, dataframe, image_format='svg', basename=None, close_bubbles=False) -> None:
+        super().__init__(dataframe, image_format, basename)
+        self.close_bubbles = close_bubbles
+        self.inverted_dataframe = invert_dataframe(self.dataframe)
 
 
-def plot_stairs_indexogram(df, img_format='svg', outfile=None):
-    inverted = invert_dataframe(df)
+class SimplePartitiogramPlotter(AbstractPartitiogramPlotter):
+    def __init__(self, dataframe, image_format='svg', basename=None, with_labels=True) -> None:
+        self.name = 'simple-partitiogram'
+        super().__init__(dataframe, image_format, basename, with_labels)
 
-    ind = ['({}, {})'.format(a, b) for a, b in inverted.index.values]
-    size = len(ind)
-    step = int(size / 8)
+    def plot(self):
+        seq = [
+            [partition, len(_df), _df.Agglomeration.iloc[0], _df.Dispersion.iloc[0]]
+            for partition, _df in self.dataframe.groupby('Partition')
+        ]
+        columns=['Partition', 'Quantity', 'Agglomeration', 'Dispersion']
+        df = pandas.DataFrame(seq, columns=columns)
 
-    plt.clf()
-    plt.stairs(inverted.Dispersion.values[:-1], ind)
-    plt.stairs(inverted.Agglomeration.values[:-1], ind)
-    plt.xticks(range(0, size, step))
-    plt.xlabel('Positions (measure, offset)')
-    plt.ylabel('Values\n<- aggl./disp. ->')
-    plt.grid()
-    plt.legend(inverted.columns)
-    plt.xticks(rotation=90)
-    plt.tight_layout()
+        plt.clf()
+        ax = df.plot(
+            grid=True,
+            kind='scatter',
+            x='Agglomeration',
+            y='Dispersion',
+        )
 
-    if img_format == 'svg':
-        plt.savefig(outfile)
-    else:
-        plt.savefig(outfile, dpi=RESOLUTION)
-    plt.close()
+        if self.with_labels:
+            factor = 1.025
+            fontsize = 12
+
+            for _, s in df.iterrows():
+                x = s['Agglomeration']
+                y = s['Dispersion']
+                v = s['Partition']
+                plt.text(x * factor, y * factor , v, fontsize=fontsize)
+
+        self.save()
 
 
-def plot_combined_indexogram(df, img_format='svg', outfile=None):
-    inverted = invert_dataframe(df)
-    series = inverted.Dispersion + inverted.Agglomeration
+class SimpleIndexogramPlotter(AbstractIndexogramPlotter):
+    def __init__(self, dataframe, image_format='svg', basename=None, close_bubbles=False) -> None:
+        self.name = 'simple-indexogram'
+        super().__init__(dataframe, image_format, basename, close_bubbles)
 
-    plt.clf()
-    ax = series.plot(grid=True)
-    ax.set_ylabel('Values\n(d - a)')
-    ax.set_xlabel('Positions (measure, offset)')
+    def plot(self):
+        def draw_vertical_line(row, x):
+            ymin = row[1].Agglomeration * -1
+            ymax = row[1].Dispersion
+            plt.vlines(x=x, ymin=ymin, ymax=ymax, linestyles='dotted', colors='C3')
 
-    plt.xticks(rotation=90)
-    plt.tight_layout()
+        plt.clf()
 
-    if img_format == 'svg':
-        plt.savefig(outfile)
-    else:
-        plt.savefig(outfile, dpi=RESOLUTION)
-    plt.close()
+        ax = self.inverted_dataframe.plot(grid=True)
+        ax.set_ylabel('Values\n<- aggl./disp. ->')
+        ax.set_xlabel('Positions (measure, offset)')
+
+        # draw vertical lines to close the bubbles
+        if self.close_bubbles:
+            rest_segment = False
+            last_row = None
+            for i, row in enumerate(dataframe.iterrows()):
+                _agg = row[1].Agglomeration
+                if pandas.isnull(_agg):
+                    if not rest_segment:
+                        if last_row:
+                            x = i - 1
+                            draw_vertical_line(last_row, x)
+                        rest_segment = True
+                else:
+                    if rest_segment:
+                        x = i
+                        draw_vertical_line(row, x)
+                        rest_segment = False
+                last_row = row
+
+        self.xticks_adjust()
+        self.save()
+
+
+class StemIndexogramPlotter(AbstractIndexogramPlotter):
+    def __init__(self, dataframe, image_format='svg', basename=None, close_bubbles=False) -> None:
+        self.name = 'stem-indexogram'
+        super().__init__(dataframe, image_format, basename, close_bubbles)
+
+    def plot(self):
+        ind = ['({}, {})'.format(a, b) for a, b in self.inverted_dataframe.index.values]
+        size = len(ind)
+        step = int(size / 8)
+
+        plt.clf()
+        plt.stem(ind, self.inverted_dataframe.Dispersion.values, markerfmt=' ')
+        plt.stem(ind, self.inverted_dataframe.Agglomeration.values, markerfmt=' ', linefmt='C1-')
+        plt.xticks(range(0, size, step))
+        plt.xlabel('Positions (measure, offset)')
+        plt.ylabel('Values\n<- aggl./disp. ->')
+        plt.grid()
+        plt.legend(self.inverted_dataframe.columns)
+
+        self.xticks_adjust()
+        self.save()
+
+
+class StairsIndexogramPlotter(AbstractIndexogramPlotter):
+    def __init__(self, dataframe, image_format='svg', basename=None, close_bubbles=False) -> None:
+        self.name = 'stairs-indexogram'
+        super().__init__(dataframe, image_format, basename, close_bubbles)
+
+    def plot(self):
+        ind = ['({}, {})'.format(a, b) for a, b in self.inverted_dataframe.index.values]
+        size = len(ind)
+        step = int(size / 8)
+
+        plt.clf()
+        plt.stairs(self.inverted_dataframe.Dispersion.values[:-1], ind)
+        plt.stairs(self.inverted_dataframe.Agglomeration.values[:-1], ind)
+        plt.xticks(range(0, size, step))
+        plt.xlabel('Positions (measure, offset)')
+        plt.ylabel('Values\n<- aggl./disp. ->')
+        plt.grid()
+        plt.legend(self.inverted_dataframe.columns)
+
+        self.xticks_adjust()
+        self.save()
+
+
+class CombinedIndexogramPlotter(AbstractIndexogramPlotter):
+    def __init__(self, dataframe, image_format='svg', basename=None, close_bubbles=False) -> None:
+        self.name = 'combined-indexogram'
+        super().__init__(dataframe, image_format, basename, close_bubbles)
+
+    def plot(self):
+        series = self.inverted_dataframe.Dispersion + self.inverted_dataframe.Agglomeration
+
+        plt.clf()
+        ax = series.plot(grid=True)
+        ax.set_ylabel('Values\n(d - a)')
+        ax.set_xlabel('Positions (measure, offset)')
+
+        self.xticks_adjust()
+        self.save()
 
 
 if __name__ == '__main__':
@@ -244,52 +272,41 @@ if __name__ == '__main__':
     if close_bubbles:
         close_bubbles = True
 
-    img_format = args.img_format.lower()
-    if img_format not in ['svg', 'jpg', 'png']:
+    image_format = args.img_format.lower()
+    if image_format not in ['svg', 'jpg', 'png']:
         raise CustomException('Image format must be svg, jpg or png.')
 
     fname = args.filename
 
     print('Running script on {} filename...'.format(fname))
 
-    indexogram_choices = {
-        'simple': plot_simple_indexogram,
-        'stem': plot_stem_indexogram,
-        'stairs': plot_stairs_indexogram,
-        'combined': plot_combined_indexogram,
-    }
+    indexogram_classes = [
+        SimpleIndexogramPlotter,
+        StemIndexogramPlotter,
+        StairsIndexogramPlotter,
+        CombinedIndexogramPlotter
+    ]
 
     try:
-        df = make_dataframe(fname)
-        bname = fname.rstrip('.csv')
+        dataframe = make_dataframe(fname)
+        basename = fname.rstrip('.csv')
 
-        partitiogram_name = bname + '-partitiogram.' + img_format
-        plot_simple_partitiogram(df, img_format, outfile=partitiogram_name)
+        SimplePartitiogramPlotter(dataframe, image_format, basename).plot()
 
         if args.all:
-            for k, fn in indexogram_choices.items():
-                outfile = bname + '-indexogram-{}.'.format(k) + img_format
-                fn(df, img_format, outfile=outfile)
+            for cls in indexogram_classes:
+                cls(dataframe, image_format, basename).plot()
         elif args.stem:
-            k = 'stem'
-            fn = indexogram_choices[k]
-            outfile = bname + '-indexogram-{}.'.format(k) + img_format
-            fn(df, img_format, outfile=outfile)
+            StemIndexogramPlotter(dataframe, image_format, basename).plot()
         elif args.stairs:
-            k = 'stairs'
-            fn = indexogram_choices[k]
-            outfile = bname + '-indexogram-{}.'.format(k) + img_format
-            fn(df, img_format, outfile=outfile)
+            StairsIndexogramPlotter(dataframe, image_format, basename).plot()
         elif args.combined:
-            k = 'combined'
-            fn = indexogram_choices[k]
-            outfile = bname + '-indexogram-{}.'.format(k) + img_format
-            fn(df, img_format, outfile=outfile)
+            CombinedIndexogramPlotter(dataframe, image_format, basename).plot()
         else:
-            k = 'simple'
-            fn = indexogram_choices[k]
-            outfile = bname + '-indexogram.' + img_format
-            fn(df, img_format, outfile=outfile, close_bubbles=close_bubbles)
+            close_bubbles = False
+            if args.close_bubbles:
+                close_bubbles = True
+            SimpleIndexogramPlotter(dataframe, image_format, basename, close_bubbles).plot()
 
     except:
         raise CustomException('Something wrong with given csv file.')
