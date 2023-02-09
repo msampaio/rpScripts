2e691e810fc03b33f370af2a0871d2f596c0c6f7
Marcos da Silva Sampaio <m@git.sampaio.me>
2023-02-09 14:17:30 -0300

    Add bubble and comparative partitiograms

--- src/rpp.py
+++ src/rpp.py
@@ -1,4 +1,5 @@
 from fractions import Fraction
+import itertools
 from matplotlib import pyplot as plt
 import argparse
 import pandas
@@ -74,6 +75,41 @@ def invert_dataframe(df):
     return inverted
 
 
+def clean_filename(s):
+    '''Clean given filename.'''
+
+    PAIRS = [
+        (' - ', '-'),
+        (' ', '-'),
+        ('_', '-'),
+        ('ç', 'c'),
+        ('á', 'a'),
+        ('à', 'a'),
+        ('ã', 'a'),
+        ('â', 'a'),
+        ('ä', 'a'),
+        ('é', 'e'),
+        ('ê', 'e'),
+        ('è', 'e'),
+        ('ê', 'e'),
+        ('í', 'i'),
+        ('ó', 'o'),
+        ('ò', 'o'),
+        ('õ', 'o'),
+        ('ô', 'o'),
+        ('ö', 'o'),
+        ('ú', 'u'),
+        ('ù', 'u'),
+        ('ü', 'u'),
+    ]
+
+    s = str(s).lower()
+
+    for a, b in PAIRS:
+        s = s.replace(a, b)
+    return s
+
+
 class AbstractPlotter(object):
     def __init__(self, dataframe, image_format='svg', basename=None) -> None:
         self.dataframe = dataframe
@@ -102,6 +138,14 @@ class AbstractPartitiogramPlotter(AbstractPlotter):
         super().__init__(dataframe, image_format, basename)
         self.with_labels = with_labels
 
+    def _make_dataframe(self):
+        seq = [
+            [partition, len(_df), _df.Agglomeration.iloc[0], _df.Dispersion.iloc[0]]
+            for partition, _df in self.dataframe.groupby('Partition')
+        ]
+        columns=['Partition', 'Quantity', 'Agglomeration', 'Dispersion']
+        return pandas.DataFrame(seq, columns=columns)
+
 
 class AbstractIndexogramPlotter(AbstractPlotter):
     def __init__(self, dataframe, image_format='svg', basename=None, close_bubbles=False) -> None:
@@ -116,13 +160,7 @@ class SimplePartitiogramPlotter(AbstractPartitiogramPlotter):
         super().__init__(dataframe, image_format, basename, with_labels)
 
     def plot(self):
-        seq = [
-            [partition, len(_df), _df.Agglomeration.iloc[0], _df.Dispersion.iloc[0]]
-            for partition, _df in self.dataframe.groupby('Partition')
-        ]
-        columns=['Partition', 'Quantity', 'Agglomeration', 'Dispersion']
-        df = pandas.DataFrame(seq, columns=columns)
-
+        df = self._make_dataframe()
         plt.clf()
         ax = df.plot(
             grid=True,
@@ -142,6 +180,114 @@ class SimplePartitiogramPlotter(AbstractPartitiogramPlotter):
                 plt.text(x * factor, y * factor , v, fontsize=fontsize)
 
         self.save()
+
+
+class BubblePartitiogramPlotter(AbstractPartitiogramPlotter):
+    def __init__(self, dataframe, image_format='svg', basename=None, with_labels=True, bubble_size=2000) -> None:
+        self.name = 'bubble-partitiogram'
+        super().__init__(dataframe, image_format, basename, with_labels)
+        self.bubble_size = bubble_size
+
+    def plot(self):
+        df = self._make_dataframe()
+        df['Quantity'] = df['Quantity'] * self.bubble_size / df['Quantity'].sum()
+
+        plt.clf()
+        ax = df.plot(
+            grid=True,
+            kind='scatter',
+            x='Agglomeration',
+            y='Dispersion',
+            s='Quantity',
+        )
+
+        if self.with_labels:
+            for _, s in df.iterrows():
+                x = s['Agglomeration']
+                y = s['Dispersion']
+                v = s['Partition']
+                plt.text(x * DISTANCE, y * DISTANCE , v, fontsize=LABELS_SIZE)
+
+        self.save()
+
+
+class ComparativePartitiogramPlotter(AbstractPartitiogramPlotter):
+    def __init__(self, dataframe, column_1, column_2, image_format='svg', basename=None, with_labels=True) -> None:
+        self.column_1 = column_1
+        self.column_2 = column_2
+        self.name = 'comparison-partitiogram-{}-{}'.format(clean_filename(self.column_1), clean_filename(self.column_2))
+        super().__init__(dataframe, image_format, basename, with_labels)
+
+    def plot(self):
+        dic = {
+            column: set(self.dataframe[self.dataframe.Label==column].Partition.unique())
+            for column in [self.column_1, self.column_2]
+        }
+
+        g1 = dic[self.column_1] - dic[self.column_2]
+        g1_label = 'Only in {}'.format(self.column_1)
+
+        g2 = dic[self.column_2] - dic[self.column_1]
+        g2_label = 'Only in {}'.format(self.column_2)
+
+        g3_label = 'Both: {} and {}'.format(self.column_1, self.column_2)
+
+        partition_map = {}
+
+        for _, row in self.dataframe.iterrows():
+            partition = row['Partition']
+            if partition not in partition_map:
+                partition_map[partition] = {
+                    'Agglomeration': row['Agglomeration'],
+                    'Dispersion': row['Dispersion'],
+                }
+
+        df = []
+        for k, v in partition_map.items():
+            if k in g1:
+                group = g1_label
+            elif k in g2:
+                group = g2_label
+            else:
+                group = g3_label
+            df.append({
+                'Partition': k,
+                'Agglomeration': v['Agglomeration'],
+                'Dispersion': v['Dispersion'],
+                'label': group
+            })
+
+        df = pandas.DataFrame(df)
+
+        fig, ax = plt.subplots(figsize=(8,6))
+        colors = ['orange', 'red', 'blue']
+
+        for n, grp in df.groupby('label'):
+            ax.scatter(
+                x='Agglomeration',
+                y='Dispersion',
+                data=grp,
+                label=n,
+                s=COMPARISON_PARTITIOGRAM_SIZE,
+                c=colors.pop(),
+            )
+
+        if len(df.Partition.unique()) < MAXIMUM_POINTS_TO_LABEL:
+            for _, s in df.iterrows():
+                x = s['Agglomeration']
+                y = s['Dispersion']
+                v = s['Partition']
+                plt.text(x * DISTANCE, y * DISTANCE , v, fontsize=LABELS_SIZE)
+
+        plt.grid()
+        plt.xlabel('Agglomeration')
+        plt.ylabel('Dispersion')
+        ax.legend(title="Label")
+
+        fig.set_size_inches(*IMG_SIZE)
+
+        self.save()
+
 
 
 class SimpleIndexogramPlotter(AbstractIndexogramPlotter):
@@ -247,6 +393,24 @@ class CombinedIndexogramPlotter(AbstractIndexogramPlotter):
         self.save()
 
 
+class ComparativePartitiogramMaker(AbstractPartitiogramPlotter):
+    def __init__(self, dataframe, image_format='svg', basename=None, with_labels=True) -> None:
+        self.name = ''
+        self.plotters = []
+        super().__init__(dataframe, image_format, basename, with_labels)
+        if 'Label' not in self.dataframe.columns:
+            raise CustomException('There is no "labels" column in the given input file.')
+
+        self.section_labels = self.dataframe['Label'].unique()
+
+        for a, b in itertools.combinations(self.section_labels, 2):
+            cpp = ComparativePartitiogramPlotter(self.dataframe, a, b, self.image_format, self.basename, self.with_labels)
+            self.plotters.append(cpp)
+
+    def plot(self):
+        for plotter in self.plotters:
+            plotter.plot()
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
                     prog = 'rpp',
@@ -257,10 +421,17 @@ if __name__ == '__main__':
     parser.add_argument("-f", "--img_format", help = "Image format (svg, jpg or png). Default=svg", default='svg')
     parser.add_argument("-r", "--resolution", help = "PNG image resolution. Default=300", default=300)
     parser.add_argument("-a", "--all", help = "Plot all available charts", action='store_true')
-    parser.add_argument("-c", "--close_bubbles", help = "Close indexogram bubbles.", default=False, action='store_true')
+    parser.add_argument("-u", "--bubble_partitiogram", help = "Partitiogram as a bubble chart", default=False, action='store_true')
+    parser.add_argument("-w", "--without_labels", help = "Partitiogram as a bubble chart without labels", default=False, action='store_true')
+    parser.add_argument("-m", "--comparative_partitiogram", help = "Comparative partitiogram. It demands an edited csv file with 'labels' column", default=False, action='store_true')
+    parser.add_argument("-c", "--close_bubbles", help = "Indexogram with bubbles' closing lines", default=False, action='store_true')
     parser.add_argument("-e", "--stem", help = "Indexogram as a stem chart", action='store_true')
     parser.add_argument("-t", "--stairs", help = "Indexogram as a stair chart", action='store_true')
     parser.add_argument("-b", "--combined", help = "Indexogram as a combination of aglomeration and dispersion", action='store_true')
+    parser.add_argument("--maximum_points_to_label", help = "Maximum number of points to label in bubble partitiogram chart. Default=50", default=50)
+    parser.add_argument("--labels_size", help = "Labels size in bubble partitiogram chart. Default=15", default=15)
+    parser.add_argument("--labels_distance", help = "Distance between points and labels in bubble partitiogram chart. Default=1.025", default=1.025)
+    parser.add_argument("--comparative_partitiogram_size", help = "Distance between points and labels in bubble partitiogram chart. Default=3", default=3)
     args = parser.parse_args()
 
     try:
@@ -268,9 +439,20 @@ if __name__ == '__main__':
     except:
         raise CustomException('Resolution must be an integer from 0 to 1200')
 
+    # Constants
+    MAXIMUM_POINTS_TO_LABEL = float(args.maximum_points_to_label)
+    LABELS_SIZE = float(args.labels_size)
+    DISTANCE = float(args.labels_distance)
+    COMPARISON_PARTITIOGRAM_SIZE = float(args.comparative_partitiogram_size)
+    IMG_SIZE = list(map(lambda x: x*0.8, (8, 6)))
+
     close_bubbles = args.close_bubbles
     if close_bubbles:
         close_bubbles = True
+
+    with_labels = not args.without_labels
+    if with_labels:
+        with_labels = True
 
     image_format = args.img_format.lower()
     if image_format not in ['svg', 'jpg', 'png']:
@@ -291,7 +473,12 @@ if __name__ == '__main__':
         dataframe = make_dataframe(fname)
         basename = fname.rstrip('.csv')
 
-        SimplePartitiogramPlotter(dataframe, image_format, basename).plot()
+        if args.comparative_partitiogram:
+            ComparativePartitiogramMaker(dataframe, image_format, basename, with_labels).plot()
+        elif args.bubble_partitiogram or args.without_labels:
+            BubblePartitiogramPlotter(dataframe, image_format, basename, with_labels).plot()
+        else:
+            SimplePartitiogramPlotter(dataframe, image_format, basename).plot()
 
         if args.all:
             for cls in indexogram_classes:
