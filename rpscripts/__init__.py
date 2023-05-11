8d02a69ceb43436515eef12ef7fa81f34eca0198
Marcos da Silva Sampaio <m@git.sampaio.me>
2023-05-11 19:59:26 -0300

    Fix missing class

--- rpscripts/__init__.py
+++ rpscripts/__init__.py
@@ -2,7 +2,6 @@
 
 from .calculator import ParsemaeSegment
 from .lib.partition import Partition
-from .lib.partition import PartitionComparator
 from .lib.base import RPData
 
 from . import calculator
