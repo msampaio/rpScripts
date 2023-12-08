9ea3373a7d616ab3b10cbaad90916504ebd23061
Marcos da Silva Sampaio <m@git.sampaio.me>
2023-12-08 10:04:11 -0300

    Improve statistics program
    - Add Statistic auxiliary class

--- rpscripts/stats.py
+++ rpscripts/stats.py
@@ -1,58 +1,59 @@
 '''This module provides a few statistical data about the given filename.'''
 
+from copy import deepcopy
 from matplotlib import pyplot as plt
 import pandas
 from .lib.base import GeneralSubparser, RPData, file_rename
 
 
-def main(filename: str, no_plot: bool, split_labels: bool) -> None:
-    '''Make histogram and print statistical summary of agglomeration and dispersion indexes.'''
+class Statistics(object):
+    '''Auxiliary class for statistics calculus.'''
 
-    rp_data = RPData(filename)
-    df = pandas.DataFrame(rp_data.data)
-    cols = ['Agglomeration', 'Dispersion']
+    def __init__(self, rpdata: RPData, name: str, image_format='svg') -> None:
+        self.rpdata = deepcopy(rpdata)
+        self.image_format = image_format
+        self.name = name
+        self.outname = file_rename(self.rpdata.path, self.image_format, self.name)
+        self.subplots = None
+        self.dataframe = pandas.DataFrame(rpdata.data)
 
-    print('Statistical summary: full')
-    print(df[cols].describe().round(2))
+    def get_histograms(self, no_plot:bool, split_labels: bool) -> None:
+        '''Make histogram and print statistical summary of agglomeration and dispersion indexes.'''
 
-    if not no_plot:
-        axes = df[cols].hist()
-        for c, ax in zip(cols, axes[0]):
-            ax.set_xlabel('{} index'.format(c))
-            ax.set_ylabel('Number of events')
+        cols = ['Agglomeration', 'Dispersion']
 
-        plt_filename = file_rename(filename, 'svg', 'histogram')
-
-        print('Saving file {}...'.format(plt_filename))
-        plt.savefig(plt_filename)
-
-    if split_labels and rp_data.labels:
-        df['Label'] = rp_data.labels
-        new_cols = cols[:]
-        new_cols.append('Label')
-
-        _df = df[new_cols].groupby('Label')
-        for label, _df in _df:
-            print('\nLabel: {}'.format(label))
-            print(_df.describe().round(2))
+        print('Statistical summary: full')
+        print(self.dataframe[cols].describe().round(2))
 
         if not no_plot:
-            for c in cols:
-                plt.clf()
-                _df = df[[c, 'Label']]
-                _df.plot.box(column=c, by='Label', grid=True)
-                plt.ylabel('{} index'.format(c))
-                plt.title('')
-                plt.xlabel('Labels')
-                plt.tight_layout()
-                plt.savefig(file_rename(filename, 'svg', 'label-{}-boxplot').format(c.lower()))
+            axes = self.dataframe[cols].hist()
+            for c, ax in zip(cols, axes[0]):
+                ax.set_xlabel('{} index'.format(c))
+                ax.set_ylabel('Number of events')
 
-                # plt.clf()
-                # grouped = df[[c, 'Label']].groupby('Label')
-                # ax = grouped.boxplot(subplots=False, rot=90)
-                # plt.ylabel('{} index'.format(c))
-                # plt.tight_layout()
-                # ax.get_figure().savefig(file_rename(filename, 'svg', 'label-{}-boxplot').format(c.lower()))
+            print('Saving file {}...'.format(self.outname))
+            plt.savefig(self.outname)
+
+        if split_labels and self.rpdata.labels:
+            self.dataframe['Label'] = self.rpdata.labels
+            new_cols = cols[:]
+            new_cols.append('Label')
+
+            _df = self.dataframe[new_cols].groupby('Label')
+            for label, _df in _df:
+                print('\nLabel: {}'.format(label))
+                print(_df.describe().round(2))
+
+            if not no_plot:
+                for c in cols:
+                    plt.clf()
+                    _df = self.dataframe[[c, 'Label']]
+                    _df.plot.box(column=c, by='Label', grid=True)
+                    plt.ylabel('{} index'.format(c))
+                    plt.title('')
+                    plt.xlabel('Labels')
+                    plt.tight_layout()
+                    plt.savefig(file_rename(self.outname, 'svg', 'label-{}-boxplot').format(c.lower()))
 
 
 class Subparser(GeneralSubparser):
@@ -68,4 +69,7 @@ class Subparser(GeneralSubparser):
         self.parser.add_argument("-l", "--labels", help = "Split labels", action='store_true')
 
     def handle(self, args):
-        main(args.filename, args.no_plot, args.labels)
+        rpdata = RPData(args.filename)
+
+        statistics = Statistics(rpdata, 'histogram', 'svg')
+        statistics.get_histograms(args.no_plot, args.labels)
