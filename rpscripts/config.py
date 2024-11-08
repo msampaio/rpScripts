c76e7aabd6e0f86d083fe4ff4c4ac000e54a55ef
Marcos da Silva Sampaio <m@git.sampaio.me>
2024-11-08 18:45:27 -0300

    Improve Lattice map setup

--- rpscripts/config.py
+++ rpscripts/config.py
@@ -4,19 +4,28 @@ These variables are used in multiple modules.'''
 
 
 import os
+import shutil
+
+import rpscripts
+
+lattice_map_filename = 'lattice_map.json'
 
 ENCODING = 'utf-8'
 HOME_DIR = os.path.expanduser('~')
 AUX_NAME = 'rps_aux'
 AUX_DIR = os.path.join(HOME_DIR, AUX_NAME)
-
-if not os.path.exists(AUX_DIR):
-    print('Creating the folder {}...'.format(AUX_DIR))
-    os.mkdir(AUX_DIR)
-
-lattice_map_filename = 'lattice_map.json'
-
 LATTICE_MAP_PATH = os.path.join(AUX_DIR, lattice_map_filename)
 
+# Copy lattice map it it doesn't exist
+if not os.path.exists(LATTICE_MAP_PATH):
+    if not os.path.exists(AUX_DIR):
+        print('Creating the folder {}...'.format(AUX_DIR))
+        os.mkdir(AUX_DIR)
+
+    print('Setting up lattice map...')
+    mpath = os.path.dirname(os.path.abspath(rpscripts.__file__))
+    orig_dir = os.path.join(mpath, 'data')
+    shutil.copy(os.path.join(orig_dir, lattice_map_filename), AUX_DIR)
+
 # Remove date and time for stable versions
-VERSION = '2.2'
+VERSION = '2.2-2024-11-08T21:42Z'
